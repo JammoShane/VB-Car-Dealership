cf80962c5656a3496414fdb93d2b6436ac6872c0
Jathniel03 <115353222+Jathniel03@users.noreply.github.com>
2022-10-22 23:57:03 +0800

    Add files via upload

---
+++ DeleteCars.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox
+Imports System.Data.SqlClient
+
+
+
+Public Class DeleteCars
+    Dim connection As New SqlConnection("Server=localhost\SQLEXPRESS ; Database=CarInformation ; Integrated Security=True")
+    Public Sub ExecuteQuery(query As String)
+
+        Dim command As New SqlCommand(query, connection)
+
+        connection.Open()
+
+        command.ExecuteNonQuery()
+
+        connection.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles txtDelete.Click
+
+
+
+        Dim deleteQuery As String = " DELETE FROM Cars Where CarId ='" & txtDeleteData.Text & "'"
+
+        ExecuteQuery(deleteQuery)
+        MessageBox.Show("Car Deleted")
+
+    End Sub
+
+
+End Class
