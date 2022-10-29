73737075669a86c41eda7ae4a1ea166f2ed17bb2
Harish Ramachandra <75201680+MisterChief273@users.noreply.github.com>
2022-10-29 18:55:03 +0800

    Add files via upload
    update files

---
+++ UpdateCars.Designer.vb
@@ -0,0 +1,207 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class UpdateCars
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.txtCarHorsepower = New System.Windows.Forms.TextBox()
+        Me.txtCarEngineSize = New System.Windows.Forms.TextBox()
+        Me.txtCarBrandName = New System.Windows.Forms.TextBox()
+        Me.txtCarModelName = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtCarID = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtCarPrice = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(302, 410)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(115, 58)
+        Me.Button2.TabIndex = 23
+        Me.Button2.Text = "Exit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'txtCarHorsepower
+        '
+        Me.txtCarHorsepower.Location = New System.Drawing.Point(442, 279)
+        Me.txtCarHorsepower.Name = "txtCarHorsepower"
+        Me.txtCarHorsepower.Size = New System.Drawing.Size(225, 22)
+        Me.txtCarHorsepower.TabIndex = 22
+        '
+        'txtCarEngineSize
+        '
+        Me.txtCarEngineSize.Location = New System.Drawing.Point(442, 223)
+        Me.txtCarEngineSize.Name = "txtCarEngineSize"
+        Me.txtCarEngineSize.Size = New System.Drawing.Size(225, 22)
+        Me.txtCarEngineSize.TabIndex = 21
+        '
+        'txtCarBrandName
+        '
+        Me.txtCarBrandName.Location = New System.Drawing.Point(442, 167)
+        Me.txtCarBrandName.Name = "txtCarBrandName"
+        Me.txtCarBrandName.Size = New System.Drawing.Size(225, 22)
+        Me.txtCarBrandName.TabIndex = 20
+        '
+        'txtCarModelName
+        '
+        Me.txtCarModelName.Location = New System.Drawing.Point(442, 109)
+        Me.txtCarModelName.Name = "txtCarModelName"
+        Me.txtCarModelName.Size = New System.Drawing.Size(225, 22)
+        Me.txtCarModelName.TabIndex = 19
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.Transparent
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(134, 271)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(276, 29)
+        Me.Label5.TabIndex = 18
+        Me.Label5.Text = "Enter Car Horsepower: "
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(134, 215)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(270, 29)
+        Me.Label4.TabIndex = 17
+        Me.Label4.Text = "Enter Car Engine Size:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(134, 159)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(283, 29)
+        Me.Label3.TabIndex = 16
+        Me.Label3.Text = "Enter Car Brand Name: "
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(134, 101)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(285, 29)
+        Me.Label2.TabIndex = 15
+        Me.Label2.Text = "Enter Car Model Name: "
+        '
+        'txtCarID
+        '
+        Me.txtCarID.Location = New System.Drawing.Point(442, 49)
+        Me.txtCarID.Name = "txtCarID"
+        Me.txtCarID.Size = New System.Drawing.Size(225, 22)
+        Me.txtCarID.TabIndex = 14
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(139, 410)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(122, 58)
+        Me.Button1.TabIndex = 13
+        Me.Button1.Text = "Update"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(134, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(161, 43)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "Enter Car ID:"
+        '
+        'txtCarPrice
+        '
+        Me.txtCarPrice.Location = New System.Drawing.Point(442, 333)
+        Me.txtCarPrice.Name = "txtCarPrice"
+        Me.txtCarPrice.Size = New System.Drawing.Size(225, 22)
+        Me.txtCarPrice.TabIndex = 25
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.BackColor = System.Drawing.Color.Transparent
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(134, 326)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(192, 29)
+        Me.Label6.TabIndex = 24
+        Me.Label6.Text = "Enter Car Price:"
+        '
+        'UpdateCars
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.CarDatabase.My.Resources.Resources.vb_bg_transformed
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(922, 542)
+        Me.Controls.Add(Me.txtCarPrice)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.txtCarHorsepower)
+        Me.Controls.Add(Me.txtCarEngineSize)
+        Me.Controls.Add(Me.txtCarBrandName)
+        Me.Controls.Add(Me.txtCarModelName)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtCarID)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "UpdateCars"
+        Me.Text = "UpdateCars"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button2 As Button
+    Friend WithEvents txtCarHorsepower As TextBox
+    Friend WithEvents txtCarEngineSize As TextBox
+    Friend WithEvents txtCarBrandName As TextBox
+    Friend WithEvents txtCarModelName As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtCarID As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtCarPrice As TextBox
+    Friend WithEvents Label6 As Label
+End Class
---
+++ UpdateCars.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Data.SqlClient
+
+Public Class UpdateCars
+    Dim connection As New SqlConnection("Server=localhost\SQLEXPRESS ; Database=CarInformation ; Integrated Security=True")
+    Public Sub ExecuteQuery(query As String)
+
+        Dim command As New SqlCommand(query, connection)
+
+        connection.Open()
+
+        command.ExecuteNonQuery()
+
+        connection.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim updateQuery As String = "UPDATE cars set CarId, Name, Brand, Engine, Horsepower = " & txtCarID.Text & "', '" & txtCarModelName.Text & "',
+        '" & txtCarBrandName.Text & "', '" & txtCarHorsepower.Text & "', '" & txtCarHorsepower.Text & "'"
+
+        ExecuteQuery(updateQuery)
+
+        MessageBox.Show("Car Updated")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+End Class
+
